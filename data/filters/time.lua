43e08738199fe655d019a3c8cdab8e64abb9ae2b
Albert Krewinkel <albert@zeitkraut.de>
2021-11-12 14:12:58 +0100

    Format submitted, published metadata fields

--- data/filters/time.lua
+++ data/filters/time.lua
@@ -4,5 +4,9 @@ function Meta (meta)
   meta.day = meta.day or tostring(curtime.day)
   meta.month = meta.month or tostring(curtime.month)
   meta.year = meta.year or tostring(curtime.year)
+
+  local date_format = '%d %B %Y'
+  meta.submitted = os.date(date_format, os.time(meta.submitted))
+  meta.published = os.date(date_format, os.time(meta.published))
   return meta
 end
