803bbad4f89df4e5cc6a0b96192eaf7286927d2d
Albert Krewinkel <albert@zeitkraut.de>
2021-11-15 11:57:33 +0100

    Allow passing article info file via `-m` command line option

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,39 +1,47 @@
 #!/bin/sh
 # Get target formats. Default is to generate both JATS and PDF.
-outformats=jats,pdf
-
 usage()
 {
-    printf "Usage: $0 [-o OUTPUT_FORMATS] <INPUT_FILE>"
+    printf "Usage: %s [-m ARTICLE_INFO_FILE] [-o OUTPUT_FORMATS] INPUT_FILE" \
+           "$0"
 }
 
 # args=$(getopt ':')
-set -- $(getopt ':o:' "$@")
+set -- $(getopt ':o:m:' "$@")
 if [ $? -ne 0 ]; then
     usage && exit 1
 fi
+
+outformats=jats,pdf
+article_info_file=
+
 while true; do
     case "$1" in
-        (-o) outformats="${2}"; shift 2;;
+        (-o)
+            outformats="${2}";
+            shift 2
+            ;;
+        (-m)
+            # we switch the directory later, so get the absolute path.
+            article_info_file="$(realpath "$2")";
+            shift 2
+            ;;
         (--) shift; break;;
         (*) usage; exit 1;;
     esac
 done
-# shift $((OPTIND - 1))
 
 # The first argument must always be the path to the main paper
 # file. The working directory is switched to the folder that the
 # paper file is in.
-input_file="$(basename "$1")"
-input_dir="$(dirname "$1")"
+input_path="$(realpath "$1")"
+input_file="$(basename "$input_path")"
+input_dir="$(dirname "$input_path")"
+shift 1
 
-# The second argument is optional, it may be the path to the article
-# info (metadata) file.
-if [ -z "$2" ]; then
-    article_info_file=/dev/null
-else
-    # we switch the directory later, so get the absolute path.
-    article_info_file="$(realpath $2)"
+# Option passed to pandoc so the article metadata is included (if given).
+if [ ! -z "$article_info_file" ]; then
+    article_info_option="--defaults=${article_info_file}"
 fi
 
 # All paths in the document are expected to be relative to the paper
@@ -46,9 +54,12 @@ for format in $(printf "%s" "$outformats" | sed -e 's/,/ /g'); do
         --defaults=shared \
         --defaults="${format}" \
         --defaults="$OPENJOURNALS_PATH"/"${JOURNAL}"/defaults.yaml \
-	      --resource-path=.:${OPENJOURNALS_PATH} \
+        --defaults="$OPENJOURNALS_PATH"/"${JOURNAL}"/defaults.yaml \
+        $article_info_option \
+	      --resource-path=.:${input_dir}:${OPENJOURNALS_PATH} \
 	      --metadata-file=${OPENJOURNALS_PATH}/${JOURNAL}/journal-metadata.yaml \
 	      --variable="${JOURNAL}" \
         --output="paper.${format}" \
+        "$input_file" \
         "$@" || exit 1
 done
