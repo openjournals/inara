34fea020bac92c9a3af9fef6455af17180eab324
Albert Krewinkel <albert@zeitkraut.de>
2021-12-22 12:15:12 +0100

    Improve crossref output

--- data/filters/inline-cited-references.lua
+++ data/filters/inline-cited-references.lua
@@ -1,5 +1,12 @@
 function Pandoc (doc)
   doc.meta.references = pandoc.utils.references(doc)
+  for i, ref in ipairs(doc.meta.references) do
+    for k, v in pairs(ref) do
+      if pandoc.utils.type(v) == 'Inlines' then
+        ref[k] = pandoc.utils.stringify(v)
+      end
+    end
+  end
   doc.meta.bibliography = nil  -- prevent bibliography from being parsed twice
   return doc
 end
