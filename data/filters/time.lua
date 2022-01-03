b5758b5aa1aeb28722012c3c1c5acc922c599fde
Albert Krewinkel <albert@zeitkraut.de>
2022-01-03 16:42:23 +0100

    time.lua: make code more robust

--- data/filters/time.lua
+++ data/filters/time.lua
@@ -1,3 +1,8 @@
+local sample_date = {
+  year = 1970,
+  month = 1,
+  day = 1,
+}
 local function format_date (date_table)
   local date_format = '%d %B %Y'
   return os.date(date_format, os.time(date_table))
@@ -5,7 +10,7 @@ end
 
 local function copy_string_values (tbl)
   local result = {}
-  for k, v in pairs(tbl) do
+  for k, v in pairs(tbl or {}) do
     result[k] = pandoc.utils.stringify(v)
   end
   return result
@@ -14,8 +19,8 @@ end
 function Meta (meta)
   local curtime = os.date('*t')
   meta.timestamp = meta.timestamp or os.date('%Y%m%d%H%M%S')
-  meta.submitted_parts = copy_string_values(meta.submitted)
-  meta.published_parts = copy_string_values(meta.published)
+  meta.submitted_parts = copy_string_values(meta.submitted or sample_date)
+  meta.published_parts = copy_string_values(meta.published or sample_date)
 
   meta.day = meta.published_parts.day or meta.day or tostring(curtime.day)
   meta.month = meta.published_parts.month or meta.month or tostring(curtime.month)
