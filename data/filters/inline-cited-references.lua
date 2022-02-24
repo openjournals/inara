fcde51d1c79db24e162f2cd34b1741cd0e11b814
Albert Krewinkel <albert@zeitkraut.de>
2022-02-24 15:49:26 +0100

    Crossref: trim whitespace in DOI
    Prevents validation failure if the DOI contains leading or trailing
    whitespace.

--- data/filters/inline-cited-references.lua
+++ data/filters/inline-cited-references.lua
@@ -1,3 +1,7 @@
+local function trim(s)
+  return s:gsub('^%s*', ''):gsub('%s*$', '')
+end
+
 function Pandoc (doc)
   doc.meta.references = pandoc.utils.references(doc)
   for i, ref in ipairs(doc.meta.references) do
@@ -8,6 +12,9 @@ function Pandoc (doc)
     end
     if ref.type == 'book' then
       ref.isbook = true
+    end
+    if ref.doi then
+      ref.doi = trim(pandoc.utils.stringify(ref.doi))
     end
   end
   doc.meta.bibliography = nil  -- prevent bibliography from being parsed twice
