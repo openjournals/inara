4f1f0fe285061511467a34124cf111235f289209
Albert Krewinkel <albert@zeitkraut.de>
2022-03-04 14:38:56 +0100

    Crossref: include unstructured_citation element to citations

---
+++ data/filters/flatten-references.lua
@@ -0,0 +1,40 @@
+function make_placeholder_doc(meta, reference)
+  local tmpmeta = {
+    ['references'] = {reference},
+    ['nocite'] = {pandoc.Cite({}, {pandoc.Citation('*', "NormalCitation")})},
+    ['csl'] = meta.csl,
+    ['citation-abbreviations'] = meta['citation-abbreviations'],
+    ['lang'] = meta['lang'],
+    ['notes-after-punctuation'] = meta['notes-after-punctuation'],
+  }
+  return pandoc.Pandoc({}, tmpmeta)
+end
+
+local function make_unstructured_citation(meta, reference)
+  local resource_path = table.concat(
+    PANDOC_STATE.resource_path,
+    pandoc.path.search_path_separator
+  )
+  local tmpdoc = pandoc.utils.run_json_filter(
+    make_placeholder_doc(meta, reference),
+    'pandoc',
+    {'--from=json', '--citeproc', '--to=json',
+     '--resource-path=' .. resource_path}
+  )
+  return pandoc.utils.stringify(tmpdoc.blocks)
+end
+
+function Meta (meta)
+  for i, ref in ipairs(meta.references) do
+    for k, v in pairs(ref) do
+      if pandoc.utils.type(v) == 'Inlines' then
+        ref[k] = pandoc.utils.stringify(v)
+      end
+    end
+    if ref.type == 'book' then
+      ref.isbook = true
+    end
+    ref.unstructured_citation = make_unstructured_citation(meta, ref)
+  end
+  return meta
+end
--- data/filters/inline-cited-references.lua
+++ data/filters/inline-cited-references.lua
@@ -5,14 +5,6 @@ end
 function Pandoc (doc)
   doc.meta.references = pandoc.utils.references(doc)
   for i, ref in ipairs(doc.meta.references) do
-    for k, v in pairs(ref) do
-      if pandoc.utils.type(v) == 'Inlines' then
-        ref[k] = pandoc.utils.stringify(v)
-      end
-    end
-    if ref.type == 'book' then
-      ref.isbook = true
-    end
     if ref.doi then
       ref.doi = trim(pandoc.utils.stringify(ref.doi))
     end
