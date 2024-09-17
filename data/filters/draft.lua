9fc67cfa941e081f4d62415b15ae8b2d38053c9d
Charles Tapley Hoyt <cthoyt@gmail.com>
2024-09-17 09:58:40 +0200

    Update draft.lua

--- data/filters/draft.lua
+++ data/filters/draft.lua
@@ -4,14 +4,11 @@
 --- resources/default-article-info.yaml
 function Meta (meta)
   if meta.draft and meta.draft ~= '' then
-    meta.cthdraft = true
     meta.article.doi = '10.xxxxxx/draft'
     meta.article.issue = '¿ISSUE?'
     meta.article.volume = '¿VOL?'
     meta.published = 'unpublished'
     meta.published_parts = os.date('*t')
     return meta
-  else
-    meta.cthdraft = false
   end
 end
