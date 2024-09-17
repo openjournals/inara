5081af737524c0a6ebc6fe9e3eb90ca82977cc8b
Charles Tapley Hoyt <cthoyt@gmail.com>
2024-09-17 09:57:27 +0200

    Update draft filter

--- data/filters/draft.lua
+++ data/filters/draft.lua
@@ -1,11 +1,17 @@
 --- Removes and alters metadata for draft output
+
+--- If you change this, please keep it in sync with
+--- resources/default-article-info.yaml
 function Meta (meta)
   if meta.draft and meta.draft ~= '' then
+    meta.cthdraft = true
     meta.article.doi = '10.xxxxxx/draft'
-    meta.article.issue = '0'
-    meta.article.volume = '0'
+    meta.article.issue = '¿ISSUE?'
+    meta.article.volume = '¿VOL?'
     meta.published = 'unpublished'
     meta.published_parts = os.date('*t')
     return meta
+  else
+    meta.cthdraft = false
   end
 end
